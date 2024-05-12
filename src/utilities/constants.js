b54a160e82065f01df4b047fa3ede9f42acdf321
Idahosa Joshua <idahosajoshua61@gmail.com>
2024-05-12 18:45:04 +0100

    cleanups

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -44,8 +44,7 @@ export const settings = {
       breakpoint: 1024,
       settings: {
         slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
+        slidesToScroll: 1,
       },
     },
     {
